257dfc1ed0c6af950420fb5e17033545250bb84b
myuxinchen <myuxinchen@outlook.com>
2026-05-03 18:51:33 +0800

    add test.java

---
+++ test1/test.java
@@ -0,0 +1,10 @@
+package test1;
+
+public class test {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        String str = "Hello, World!";
+    }
+}
+                            
